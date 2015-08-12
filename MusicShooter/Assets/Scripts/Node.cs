108879c6bbd17da7e904025447e3233b71791b0a
Tim Kofoed <timkofoed83@gmail.com>
2015-08-12 22:32:04 +0200

    Made the first test-scene

---
+++ MusicShooter/Assets/Scripts/Node.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("MouseDown on node " + this.gameObject.name);
+    }
+
+    public void OnEnd()
+    {
+        Destroy(this.transform.root.gameObject);
+    }
+}
